fbc26cfeae4b06d4cf62c48152a1d9ff93d28c3c
Aliwoto <woto@kaizoku.cyou>
2022-10-06 12:02:13 +0330

    Load handlers defined in helpPlugin as well
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- src/plugins/handlers.go
+++ src/plugins/handlers.go
@@ -1,6 +1,7 @@
 package plugins
 
 import (
+	"github.com/AnimeKaizoku/PsstRobot/src/plugins/helpPlugin"
 	"github.com/AnimeKaizoku/PsstRobot/src/plugins/startPlugin"
 	"github.com/AnimeKaizoku/PsstRobot/src/plugins/whisperPlugin"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
@@ -9,4 +10,5 @@ import (
 func LoadAllHandlers(d *ext.Dispatcher, triggers []rune) {
 	whisperPlugin.LoadHandlers(d, triggers)
 	startPlugin.LoadHandlers(d, triggers)
+	helpPlugin.LoadHandlers(d, triggers)
 }
