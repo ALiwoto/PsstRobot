be9a50af8f1123efd508572bdccee1766ff79f6a
Aliwoto <woto@kaizoku.cyou>
2022-10-06 10:34:06 +0330

    Add some text to help command
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- src/plugins/startPlugin/handlers.go
+++ src/plugins/startPlugin/handlers.go
@@ -85,9 +85,24 @@ func helpHandler(bot *gotgbot.Bot, ctx *ext.Context) error {
 	user := ctx.EffectiveUser
 	message := ctx.EffectiveMessage
 
-	md := mdparser.GetNormal("hi ")
+	md := mdparser.GetNormal("Hey ")
 	md.Mention(user.FirstName, user.Id)
-	md.Normal("!\nThis help section is not completed (yet).")
+	md.Normal("!\nI'm " + bot.Username + ", a whisper bot with extra" +
+		" features!\n")
+	md.Normal("Here is some of the commands you can use:\n\n")
+
+	md.Bold("• ").Mono("/create").Bold(": \n\t\t")
+	md.Normal("Prepares an advanced whisper message, all types of medias and ")
+	md.Normal("media-albums are supported.\n\n")
+	md.Bold("• ").Mono("/privacy").Bold(": \n\t\t")
+	md.Normal("Displays your current privacy mode. You can disable/enable it ")
+	md.Normal("using this same command. If your privacy is enabled, ")
+	md.Normal("you won't cause the whisper message to be edited to ")
+	md.Normal("\"").Mention(user.FirstName, user.Id).Normal(" read the whisper\".\n\n")
+	md.Bold("• ").Mono("/notifications").Bold(": \n\t\t")
+	md.Normal("Displays your current notifications preferences using a panel.")
+	md.Normal(" You can change your whisper-notifications preferences using ")
+	md.Normal("using buttons of that panel.")
 
 	_, _ = message.Reply(bot, md.ToString(), &gotgbot.SendMessageOpts{
 		ParseMode:             core.MarkdownV2,
